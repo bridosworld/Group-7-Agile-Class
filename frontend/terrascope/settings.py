bd1bc87a660b93f62d8e2ad0aca2f61b4d58488f
bridosworld <odibobright@gmail.com>
2025-12-02 13:42:28 +0000

    Core Added

--- frontend/terrascope/settings.py
+++ frontend/terrascope/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',  # Custom app for TerraSCOPE geospatial observation management
 ]
 
 MIDDLEWARE = [
