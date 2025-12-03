40597f9f7eb80ea350a47783e47e0d1c9c8f0311
Umair <umairamin9876@gmail.com>
2025-12-03 11:05:45 +0000

    (US-14, US-16)

--- frontend/terrascope/settings.py
+++ frontend/terrascope/settings.py
@@ -112,6 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "dashboard"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
